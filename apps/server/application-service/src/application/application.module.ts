e3b64da423cea2f7eea288664d21ef90f2de712c
Zsolt Molnar <zsotyooo@gmail.com>
2022-05-11 08:57:46 +0200

    refactor to nestjs monorepo style

--- apps/server/application-service/src/application/application.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ApplicationResolver } from './application.resolver';
-import { ApplicationService } from './application.service';
-
-@Module({
-  providers: [ApplicationResolver, ApplicationService],
-})
-export class ApplicationModule {}
--- apps/server/application-service/src/collaborator/collaborator.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CollaboratorResolver } from './collaborator.resolver';
-import { CollaboratorService } from './collaborator.service';
-
-@Module({
-  providers: [CollaboratorResolver, CollaboratorService],
-})
-export class CollaboratorModule {}
